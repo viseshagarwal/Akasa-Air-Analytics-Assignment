d1b97270e8fcc0db95de98477a030c862f6f1ca8
Visesh Agarwal <viseshagarwal@outlook.com>
2024-09-29 19:19:29 +0530

    Replace aviation_data_cleaned.csv with a new version; remove sql_connection.py; update normalized_data.csv for consistent data formatting

--- generate_report.py
+++ generate_report.py
@@ -1,14 +1,14 @@
+from sqlalchemy import create_engine, text
+import os
 import pandas as pd
-
-# import numpy as np
+from dotenv import load_dotenv
 import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import datetime, timedelta
-from sql_connection import *
-import os
-from dotenv import load_dotenv
 import scipy.stats as stats
+import warnings
 
+warnings.filterwarnings("ignore")
 
 load_dotenv()
 
@@ -24,56 +24,113 @@ def read_data_csv():
     return df
 
 
+messages = []
+
+
+# Missing Values
+def check_missing_values(df, messages):
+    messages.append("<h2>Handling Missing Values...</h2>")
+
+    # Missing values before handling
+    missing_before = df.isnull().sum()
+    messages.append("<h3>Missing Values Before Handling:</h3>")
+    messages.append(missing_before.to_frame().to_html())
+
+    # Handle missing values for 'DelayMinutes'
+    df["DelayMinutes"] = df["DelayMinutes"].fillna(0)
+
+    # Missing values after handling
+    missing_after = df.isnull().sum()
+    messages.append("<h3>Missing Values After Handling:</h3>")
+    messages.append(missing_after.to_frame().to_html())
+    messages.append("<br/><hr>")
+    return df
+
+
+# Check for duplicates
+def check_duplicates(df, messages):
+    messages.append("<h2>Checking for Duplicates...</h2>")
+
+    # Count duplicate entries
+    duplicate_count = df.duplicated(
+        subset=[
+            "FlightNumber",
+            "DepartureDate",
+            "DepartureTime",
+            "ArrivalDate",
+            "ArrivalTime",
+            "Airline",
+            "DelayMinutes",
+        ]
+    ).sum()
+    messages.append(
+        f"<p><strong>Number of duplicate entries:</strong> {
+            duplicate_count}</p>"
+    )
+
+    # Remove duplicates
+    df = df.drop_duplicates(
+        subset=[
+            "FlightNumber",
+            "DepartureDate",
+            "DepartureTime",
+            "ArrivalDate",
+            "ArrivalTime",
+            "Airline",
+            "DelayMinutes",
+        ]
+    )
+
+    messages.append(
+        f"<p><strong>Number of entries after removing duplicates:</strong> {
+            df.shape[0]}</p>"
+    )
+    messages.append("<br/><hr>")
+    return df
+
+
+# Convert 12-hour time format to 24-hour
 def convert_to_24hr(time_str):
     return datetime.strptime(time_str, "%I:%M %p").strftime("%H:%M")
 
 
-def clean_data(df, messages):
-    messages.append("<h2>Cleaning Data...</h2>")
+# Check for inconsistent time entries
+def check_inconsistent_time_entries(df, messages):
+    messages.append("<h2>Checking for Inconsistent Time Entries...</h2>")
 
-    # Check for missing values
-    messages.append("<h3>Missing Values Before Handling:</h3>")
-    missing_before = df.isnull().sum().to_frame().to_html()
-    messages.append(missing_before)
-
-    df["DelayMinutes"] = df["DelayMinutes"].fillna(0)
-    messages.append("<h3>Missing Values After Handling:</h3>")
-    missing_after = df.isnull().sum().to_frame().to_html()
-    messages.append(missing_after)
-
-    duplicate_count = df.duplicated().sum()
+    # Inconsistent time entries
+    inconsistent_time_entries = df[df["DepartureTime"] > df["ArrivalTime"]]
     messages.append(
-        f"<p><strong>Number of duplicate entries:</strong> {duplicate_count}</p>"
+        f"<p><strong>Number of inconsistent time entries:</strong> {
+            inconsistent_time_entries.shape[0]}</p>"
     )
 
-    # Remove duplicates
-    df = df.drop_duplicates()
+    # Remove inconsistent time entries
+    df = df[df["DepartureTime"] <= df["ArrivalTime"]]
     messages.append(
-        f"<p><strong>Number of entries after removing duplicates:</strong> {df.shape[0]}</p>"
+        f"<p><strong>Number of entries after removing inconsistent time entries:</strong> {
+            df.shape[0]}</p>"
     )
 
     # Convert DepartureTime and ArrivalTime to 24-hour format
     df["DepartureTime_24"] = df["DepartureTime"].apply(convert_to_24hr)
     df["ArrivalTime_24"] = df["ArrivalTime"].apply(convert_to_24hr)
 
+    # Combine DepartureDate and DepartureTime into a single datetime
     df["DepartureDateTime"] = pd.to_datetime(
         df["DepartureDate"] + " " + df["DepartureTime"], format="%m/%d/%Y %I:%M %p"
     )
     df["ArrivalDateTime"] = pd.to_datetime(
         df["ArrivalDate"] + " " + df["ArrivalTime"], format="%m/%d/%Y %I:%M %p"
     )
-
-    # Identify inconsistent time entries
-    inconsistent_times = df[df["ArrivalDateTime"] < df["DepartureDateTime"]]
     messages.append(
-        f"<p><strong>Number of inconsistent time entries:</strong> {inconsistent_times.shape[0]}</p>"
+        "<p>Converted DepartureTime and ArrivalTime to 24-hour format and combined with dates.</p>"
     )
-
-    df = df[df["ArrivalDateTime"] >= df["DepartureDateTime"]]
-
+    messages.append("<br/><hr>")
     return df
 
 
+# Normalize Data
 def normalize_data(df, messages):
     messages.append("<h2>Normalizing Data...</h2>")
 
@@ -95,7 +152,6 @@ def normalize_data(df, messages):
 
     # Drop the temporary 24-hour columns
     df = df.drop(["DepartureTime_24", "ArrivalTime_24"], axis=1)
-
     messages.append(
         "<p>Replaced original time columns with 24-hour format and removed temporary columns.</p>"
     )
@@ -104,27 +160,139 @@ def normalize_data(df, messages):
     df["FlightDuration"] = (
         df["ArrivalDateTime"] - df["DepartureDateTime"]
     ).dt.total_seconds() / 60
-
     messages.append("<p>Calculated FlightDuration in minutes.</p>")
 
-    df.to_csv("reports/normalized_data.csv", index=False)
     return df
 
 
-def insert_data(connection, df, messages):
+def insert_data(df, messages):
     messages.append("<h2>Inserting Data into MySQL...</h2>")
 
     # Create connection
-    connection = create_connection(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT)
+    connection_string = (
+        f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
 
-    insert_data(connection, df)
-    messages.append("<p>Data inserted into MySQL successfully.</p>")
+    try:
+        engine = create_engine(connection_string)
+        # create table if not exists
+        create_table_query = text(
+            """
+        CREATE TABLE IF NOT EXISTS aviation_data (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            FlightNumber TEXT,
+            DepartureDate TEXT,
+            DepartureTime TEXT,
+            ArrivalDate TEXT,
+            ArrivalTime TEXT,
+            Airline TEXT,
+            DelayMinutes FLOAT
+        )"""
+        )
 
-    # Fetch data back from MySQL
-    df_fetched = fetch_data(connection)
-    messages.append("<p>Data fetched from MySQL successfully.</p>")
+        with engine.connect() as connection:
+            connection.execute(create_table_query)
+
+        # insert data
+        df.to_sql("aviation_data", engine, if_exists="append", index=False)
+        messages.append("<p>Data inserted into MySQL successfully.</p>")
+
+        # fetch data
+        df_fetched = pd.read_sql("SELECT * FROM aviation_data", engine)
+        messages.append("<p>Data fetched from MySQL successfully.</p>")
+        messages.append("<br/><hr>")
+    except Exception as e:
+        print(f"Error: {e}")
+
+    finally:
+        engine.dispose()
 
     return df_fetched
+
+
+def generate_report(messages):
+    # Start HTML content
+    html_content = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Aviation Data Report</title>
+        <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 40px;
+            line-height: 1.6;
+        }
+        h1, h2, h3 {
+            color: #2E4053;
+        }
+        h1 {
+            text-align: center;
+            margin-bottom: 30px;
+        }
+        h2 {
+            margin-top: 30px;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-bottom: 20px;
+            font-size: 14px;
+        }
+        table, th, td {
+            border: 1px solid #ddd;
+        }
+        th, td {
+            padding: 10px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+        tr:hover {
+            background-color: #f1f1f1;
+        }
+        a {
+            text-decoration: none;
+            color: #2980B9;
+        }
+        a:hover {
+            text-decoration: underline;
+            color: #1A5276;
+        }
+        img {
+            max-width: 100%;
+            height: auto;
+            display: block;
+            margin-top: 10px;
+            margin-bottom: 20px;
+        }
+    </style>
+    </head>
+    <body>
+        <h1>Aviation Data Report</h1>
+    """
+
+    # Add all messages
+    for message in messages:
+        html_content += message
+
+    # Close HTML content
+    html_content += """
+    </body>
+    </html>
+    """
+
+    # Write to HTML file
+    if not os.path.exists("reports"):
+        os.makedirs("reports")
+
+    with open("reports/aviation_report.html", "w") as report_file:
+        report_file.write(html_content)
+
+    print("HTML report generated and saved as 'reports/aviation_report.html'.")
 
 
 def data_analysis(df, messages):
@@ -134,10 +302,11 @@ def data_analysis(df, messages):
     delay_summary = df["DelayMinutes"].describe()
     messages.append("<h3>Delay Minutes Summary:</h3>")
     messages.append(delay_summary.to_frame().to_html())
+    messages.append("<br/><hr>")
 
     # Plot distribution of delays
-    plt.figure(figsize=(10, 6))
-    sns.histplot(df["DelayMinutes"], bins=30, kde=True)
+    plt.figure(figsize=(8, 5))
+    sns.histplot(df["DelayMinutes"], bins=10, kde=True)
     plt.title("Distribution of Flight Delays")
     plt.xlabel("Delay Minutes")
     plt.ylabel("Frequency")
@@ -145,13 +314,22 @@ def data_analysis(df, messages):
 
     plt.savefig("reports/delay_distribution.png")
     plt.close()
+    messages.append("<center><h2>Plots:</h2></center>")
     messages.append(
         "<p>Saved plot: <a href='delay_distribution.png' target='_blank'>Distribution of Flight Delays</a></p><br/> <img src='delay_distribution.png'>"
     )
+    messages.append("<h2>Insights:</h2>")
+    messages.append(
+        "<p>1. The distribution shows that the majority of flights had delays between 0 and 10 minutes, with the highest frequency in this range.</p>")
+    messages.append("<p>2. The overall shape of the histogram suggests a right-skewed distribution. Most delays are concentrated at the lower end, with fewer flights experiencing significant delays (over 30 minutes).</p>")
+    messages.append(
+        "<p>3. A significant proportion of flights face relatively short delays (below 15 minutes).</p>")
+    messages.append("<br/><hr>")
 
     # Average delay per airline
-    average_delay_airline = df.groupby("Airline")["DelayMinutes"].mean().reset_index()
-    messages.append("<h3>Average Delay per Airline:</h3>")
+    average_delay_airline = df.groupby(
+        "Airline")["DelayMinutes"].mean().reset_index()
+    messages.append("<h2>Average Delay per Airline:</h2>")
     messages.append(average_delay_airline.to_html(index=False))
 
     # Plot average delay per airline
@@ -172,12 +350,21 @@ def data_analysis(df, messages):
     messages.append(
         "<p>Saved plot: <a href='average_delay_airline.png' target='_blank'>Average Delay by Airline</a></p><br/><img src='average_delay_airline.png'>"
     )
+    messages.append("<h2>Insights:</h2>")
+    messages.append(
+        "<p>1. The average delay times vary significantly across different airlines.</p>")
+    messages.append(
+        "<p>2. American Airlines has the highest average delay, while Delta Airline has the lowest average delay.</p>")
+    messages.append(
+        "<p>3. The difference in average delay times suggests variations in operational efficiency and performance among airlines.</p>")
+    messages.append("<br/><hr>")
 
     # Extract hour from DepartureTime
-    df["DepartureHour"] = pd.to_datetime(df["DepartureTime"], format="%H:%M").dt.hour
+    df["DepartureHour"] = pd.to_datetime(
+        df["DepartureTime"], format="%H:%M").dt.hour
 
     # Scatter plot of DepartureHour vs DelayMinutes
-    plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(8, 5))
     sns.scatterplot(
         data=df, x="DepartureHour", y="DelayMinutes", hue="Airline", alpha=0.6
     )
@@ -191,13 +378,21 @@ def data_analysis(df, messages):
     messages.append(
         "<p>Saved plot: <a href='departure_vs_delay.png' target='_blank'>Flight Delays vs Departure Time</a></p> <br/><img src='departure_vs_delay.png'>"
     )
+    messages.append("<h2>Insights:</h2>")
+    messages.append(
+        "<p>1. Evening Delays: Delays increase significantly after 16:00, especially for United and American Airlines.</p>")
+    messages.append(
+        "<p>2. Delta's Punctuality: Delta consistently has minimal delays.</p>")
+    messages.append(
+        "<p>3. American Airlines Peaks: American Airlines faces large delays around morning 8:00 and night 20:00.</p>")
+    messages.append("<br/><hr>")
 
     # Analyze average delay by departure hour
     average_delay_hour = (
         df.groupby("DepartureHour")["DelayMinutes"].mean().reset_index()
     )
 
-    plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(8, 5))
     sns.lineplot(
         data=average_delay_hour, x="DepartureHour", y="DelayMinutes", marker="o"
     )
@@ -211,6 +406,14 @@ def data_analysis(df, messages):
     messages.append(
         "<p>Saved plot: <a href='average_delay_hour.png' target='_blank'>Average Delay by Departure Hour</a></p> <br/><img src='average_delay_hour.png'>"
     )
+    messages.append("<h2>Insights:</h2>")
+    messages.append(
+        "<p> 1. High delays in the evening: Delays peak after 17:00, with the highest around 20:00 (60+ minutes).</p>")
+    messages.append(
+        "<p>2. Minimal delays at midday: Almost no delays between 14:00 and 15:00.</p>")
+    messages.append(
+        "<p>3. Morning decline: Delays gradually decrease from 9:00 to 13:00.</p>")
+    messages.append("<br/><hr>")
 
     # Perform one-way ANOVA
     airline_delays = [
@@ -221,7 +424,8 @@ def data_analysis(df, messages):
 
     messages.append("<h3>ANOVA Result:</h3>")
     messages.append(
-        f"<p>F-statistic: {anova_result.statistic:.4f}, p-value: {anova_result.pvalue:.4f}</p>"
+        f"<p>F-statistic: {anova_result.statistic:.4f}, p-value: {
+            anova_result.pvalue:.4f}</p>"
     )
 
     # Interpretation
@@ -231,78 +435,49 @@ def data_analysis(df, messages):
         interpretation = "<p>There is <strong>no significant difference</strong> in delays between airlines.</p>"
     messages.append("<h3>Interpretation:</h3>")
     messages.append(interpretation)
-
-    return delay_summary, average_delay_airline
-
-
-def generate_report(messages):
-
-    # Start HTML content
-    html_content = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Aviation Data Report</title>
-        <style>
-            body { font-family: Arial, sans-serif; margin: 40px; }
-            h1, h2, h3 { color: #2E4053; }
-            h1 {text-align: center;}
-            table { width: 100%; border-collapse: collapse; margin-bottom: 20px; }
-            table, th, td { border: 1px solid #ddd; }
-            th, td { padding: 8px; text-align: left; }
-            th { background-color: #f2f2f2; }
-            a { text-decoration: none; color: #2980B9; }
-            a:hover { text-decoration: underline; }
-        </style>
-    </head>
-    <body>
-        <h1>Aviation Data Report</h1>
-    """
-
-    # Add all messages
-    for message in messages:
-        html_content += message
-
-    # Close HTML content
-    html_content += """
-    </body>
-    </html>
-    """
-
-    # Write to HTML file
-    with open("reports/aviation_report.html", "w") as report_file:
-        report_file.write(html_content)
-
-    print("HTML report generated and saved as 'reports/aviation_report.html'.")
+    messages.append("<br/><hr>")
+    # return delay_summary, average_delay_airline
 
 
 def main():
     messages = []
 
-    # create a new folder called as reports if not exists
+    # create a new folder called reports if not exists
     if not os.path.exists("reports"):
         os.makedirs("reports")
 
     # Read data from CSV
     messages.append("<h2>Reading Data...</h2>")
     df = read_data_csv()
-    messages.append(
-        f"<p>Loaded dataset with {df.shape[0]} records and {df.shape[1]} columns.</p>"
-    )
+    messages.append(f"<p>Loaded dataset with {df.shape[0]} records and {
+                    df.shape[1]} columns.</p>")
     messages.append("<h3>Sample Data:</h3>")
     messages.append(df.head().to_html())
+    messages.append("<br/><hr>")
+    # Insert data into MySQL and fetch back
+    df = insert_data(df, messages)
 
-    # Clean the data
-    df_cleaned = clean_data(df, messages)
+    # Handle missing values
+    df = check_missing_values(df, messages)
+
+    # Check for duplicates
+    df = check_duplicates(df, messages)
+
+    # Check for inconsistent time entries
+    df = check_inconsistent_time_entries(df, messages)
 
     # Normalize the data
-    df_normalized = normalize_data(df_cleaned, messages)
-
-    # # Insert data into MySQL and fetch back
-    # df_fetched = insert_data(df_normalized, messages)
-
+    df_normalized = normalize_data(df, messages)
+    df_normalized.to_csv("reports/normalized_data.csv", index=False)
+    messages.append(
+        "<p>Normalized data saved as <a href='reports/normalized_data.csv' target='_blank'>normalized_data.csv</a>.</p>"
+    )
+    messages.append("<br/><hr>")
     # Perform data analysis
-    delay_summary, average_delay_airline = data_analysis(df_normalized, messages)
+    data_analysis(df_normalized, messages)
+
+    # Save cleaned data to CSV
+    df.to_csv("reports/aviation_data_cleaned.csv", index=False)
 
     # Generate report
     generate_report(messages)
